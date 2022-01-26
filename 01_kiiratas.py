c984e885896b9416df5cb4b5a325cfdfb886f4d4
Ákos Klobetz <klobetz@gmail.com>
2022-01-26 17:33:27 +0100

    #1 több sor

--- 01_kiiratas.py
+++ 01_kiiratas.py
@@ -1,3 +1,9 @@
 # kiíratás:
-print("Ez az első programom") # vagy
-print('Ez is szöveg')
+print("Ez az első programom") #vagy
+print('\nEz is szöveg')
+
+#több soros megjelenítés:
+print("""\nez a szöveg         
+több sorban
+jelenik meg.""")               #\n az sortörést jelenti
+
