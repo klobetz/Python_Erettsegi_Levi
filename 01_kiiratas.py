7875d8ba61a49ca2515b9d3dfe4c448d8171916e
Ákos Klobetz <klobetz@gmail.com>
2022-01-26 17:57:44 +0100

    #2 változó és értékadás

--- 01_kiiratas.py
+++ 01_kiiratas.py
@@ -7,3 +7,19 @@ print("""\nez a szöveg
 több sorban
 jelenik meg.""")               #\n az sortörést jelenti
 
+# Értéktípusok:
+15          #int
+'szöveg'    #str (string)
+3.5         #float (lebegő pontos)
+3,5         #értékpár
+
+# változók létrehozása és értékadás
+print()
+a = 'változób tárolt string'        #változó létrehozása és értékadás
+print(a)            #az a változó kiíratása a képernyőre
+
+x = 15
+y = 3.5
+z = 'szöveg'
+
+print(a,x,y,z)
