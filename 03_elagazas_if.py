e6e18eceffd74cb296ea6405150a29be1d4dbeb3
Ákos Klobetz <klobetz@gmail.com>
2022-02-23 17:39:24 +0100

    #5 operátorok használata

---
+++ 03_elagazas_if.py
@@ -0,0 +1,18 @@
+#booleam típus 2 db True és a False
+#type(True)   class 'bool'
+#type(False)  class 'bool'
+
+#ÖSSZEHASONLÍTÓ OPERÁTOROK:
+    #Egyenlő a == b
+    #Nem egyenlő: a != b            negátolás!!!!
+    #Kisebb mint: a < b
+    #Nagyobb mint: a > b
+    #Kisebb vagy egyenlő: a <= b
+    #Nagyobb vagy egyenlő: a >= b
+
+#LOGIKAI OPERÁTOROK:
+    #És => and
+    #Vagy => or
+    #tagadás => not
+
+# feltételes végrehajtás (az if használata)
