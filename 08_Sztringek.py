84e6c8a96ab3ac5d3aeb9b2da02378c0110424bd
Ákos Klobetz <klobetz@gmail.com>
2022-04-13 17:25:32 +0200

    #10 sztring második része

--- 08_Sztringek.py
+++ 08_Sztringek.py
@@ -35,3 +35,49 @@ while i < len(sztring):
     print((i+1), sztring[i])
 
     i+=1
+
+#sztring szeletelés
+gyumolcs = "szederfa"
+print(gyumolcs[0:3])            #az elejétől a 3.karakterig kiír
+print(gyumolcs[3:400])          #ez nem fut hibára kiírja végig
+
+#in not in oprátorok használata
+
+print('a' in gyumolcs)      #az a megtalálható-e a gyümölcs változóban?
+
+if 'a' in gyumolcs:
+    print("igen van benne 'a'")
+
+print('a' not in gyumolcs)
+
+if 'a' not in gyumolcs:         #csak akkor írja ki ha van else ert az értéke false
+    pass
+else:
+    print("A szóban szerele az 'a' kerekter")
+
+# kérjünk be a felhasználótól egy karaktert és ellenőrizzük le, hogy szerepel-e a szederfa szóban vagy sem
+#ha szerepel akkor írassuk kihogy hányadik helyen szerepel az adott karakter
+valasz = input("Kérek egy karaktert: ")
+
+if valasz in gyumolcs:                  #megvizsgálom hogy van e benne
+    print("Igen szerepel benne")
+    hanyadik = 0
+    for i in gyumolcs:                  #ha van benne akkor megnézem hogy hányadik
+        if valasz==i:
+            print(f"az adott karakter a {hanyadik+1} pozíción szerepel")
+            #break      #ez csak akkor kell ha csak az első találatig akarok menni
+        hanyadik+=1
+else:
+    print(f"Nem nem szerepel benne a {valasz}")
+
+
+#házi ugyanez while-al
+
+#beépített metódus használata
+print(gyumolcs.find('k'))           # ha nincs benne akkor -1 -el tér vissza
+print(gyumolcs.find('e')+1)         # ha van benne akkor kiírja az indexét és ehez hozzáadunk egyet és így pozíciót kapok
+
+#split metódus darabolás
+mondat = "Én már kezdem érteni, ezeket a műveleteket"
+print(mondat.split())
+print(type(mondat.split()))
