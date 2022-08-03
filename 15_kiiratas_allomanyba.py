4a38a77e7e1da65f1cb224e518479d1dc92a2f9e
Ákos Klobetz <klobetz@gmail.com>
2022-08-03 11:50:18 +0200

    #15 felhasználó ad fájlnevet!

--- 15_kiiratas_allomanyba.py
+++ 15_kiiratas_allomanyba.py
@@ -6,3 +6,9 @@ with open("Kiiratasok/elsokiiras.txt", "w", encoding="utf8") as f:
 szoveg = "Szia Ákos most már változóból!"
 with open("Kiiratasok/masodikkiiras.txt", "w", encoding="utf8") as f:
     f.writelines(szoveg)
+
+#felhasználó ad fájlnevet!
+#valasz = input("Mi legyen a fájl neve?: ")
+valasz = "Teszt1"
+with open(f"Kiiratasok/{valasz}.txt", "w", encoding="utf8") as f:
+    f.writelines(f"felhasználó álltal {szoveg} megadott fájlnév!!!!!!")
