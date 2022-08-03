bf11456a140fd7d4a5a0a06ad65e1c14a90dbc7a
Ákos Klobetz <klobetz@gmail.com>
2022-08-03 11:37:30 +0200

    #15 kiírás fájlba változóból

--- 15_kiiratas_allomanyba.py
+++ 15_kiiratas_allomanyba.py
@@ -1,3 +1,8 @@
 #kiírás fájlba
 with open("Kiiratasok/elsokiiras.txt", "w", encoding="utf8") as f:
     f.writelines("Szia Ákos!")
+
+#változó értékének kiírása
+szoveg = "Szia Ákos most már változóból!"
+with open("Kiiratasok/masodikkiiras.txt", "w", encoding="utf8") as f:
+    f.writelines(szoveg)
