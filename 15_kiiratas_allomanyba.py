ccb710d07d6bd3223a724f76bf33bb74795438e2
Ákos Klobetz <klobetz@gmail.com>
2022-08-03 12:11:56 +0200

    #15 lista készítése és kiírása fájlba

--- 15_kiiratas_allomanyba.py
+++ 15_kiiratas_allomanyba.py
@@ -1,4 +1,6 @@
 #kiírás fájlba
+import random
+
 with open("Kiiratasok/elsokiiras.txt", "w", encoding="utf8") as f:
     f.writelines("Szia Ákos!")
 
@@ -12,3 +14,26 @@ with open("Kiiratasok/masodikkiiras.txt", "w", encoding="utf8") as f:
 valasz = "Teszt1"
 with open(f"Kiiratasok/{valasz}.txt", "w", encoding="utf8") as f:
     f.writelines(f"felhasználó álltal {szoveg} megadott fájlnév!!!!!!")
+
+# hozz létre egy 100 elemű listát és töltsd fel véletlen számokkal! (0 és 100 közöttivel)
+lista = [random.randrange(0,101,1) for elem in range(100)]
+
+for elem in lista:
+    print(elem,end=" ")
+
+print(f"\nA lista {len(lista)} db elemet tartalmat")
+
+#rendezzük növekvőbe a listát:
+
+#rendezett = sorted(lista)
+# for elem in renbezett:
+#     print(elem,end=" ")
+
+#vagy
+
+for elem in sorted(lista):
+    print(elem,end=" ")
+
+#írjuk ki a listát egy fájlba
+with open("Kiiratasok/lista.txt", "w", encoding="utf8") as f:
+    f.writelines(str(lista))
